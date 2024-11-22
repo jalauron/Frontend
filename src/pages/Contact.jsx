c0b451a4e86802c9766e3c93065908f10ab94dfc
jalauron <jalauron@gbox.ncf.edu.ph>
2024-11-22 16:44:13 +0800

    .

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div style>
+      <h1>Contact</h1>
+      <p>Feel free to contact me!</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/Hobbies.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hobbies() {
+  return (
+    <div>
+      <h1>Hobbies</h1>
+      <p>These are my hobbies.</p>
+    </div>
+  );
+}
+
+export default Hobbies;
