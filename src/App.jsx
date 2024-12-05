2054c319ac73d3552cae96fd0eec1f4635fcadb2
jalauron <jalauron@gbox.ncf.edu.ph>
2024-12-05 08:56:54 +0800

    v7_relative...

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,20 @@ import Education from './pages/Education';
 import Hobbies from './pages/Hobbies';
 import Contact from './pages/Contact';
 
+// You can add this as part of router setup if needed, or check for build config
+const routerConfig = {
+  future: {
+    v7_relativeSplatPath: true, // This may be a custom setting, not standard in react-router
+  },
+};
+
 function App() {
   return (
     <div>
       <Router>
         <TopNav />
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Education" element={<Education />} />
